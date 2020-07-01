1ad3e79a84c38818dfbcbc0c1d2b5801cf7f6ad9
omar ghanim <omarmghanim@gmail.com>
2020-07-01 15:28:10 +0200

    Add gitignore, Procfile and update ports

--- app.js
+++ app.js
@@ -126,7 +126,10 @@ List.findOneAndDelete({name:listName},{ $pull : {items:{_id : checkedID}}},funct
 
 });
 
-
-app.listen(3000,function(){
+let port = process.env.PORT;
+if(port == null || port = ""){
+  port = 3000;
+}
+app.listen(port,function(){
   console.log("server running");
 })
