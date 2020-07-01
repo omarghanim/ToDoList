8522bedf6f2bee92333161a6f2444bd1d8af5f4b
omar ghanim <omarmghanim@gmail.com>
2020-07-01 16:11:12 +0200

    fix error

--- app.js
+++ app.js
@@ -126,10 +126,7 @@ List.findOneAndDelete({name:listName},{ $pull : {items:{_id : checkedID}}},funct
 
 });
 
-let port = process.env.PORT;
-if(port == null || port = ""){
-  port = 3000;
-}
-app.listen(port,function(){
+
+app.listen(process.env.PORT || 3000,function(){
   console.log("server running");
 })
